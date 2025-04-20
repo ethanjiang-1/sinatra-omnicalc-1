2e00789f053985fa75ae146a57d373a1fa5d2497
ethanjiang-1 <146405629+ethanjiang-1@users.noreply.github.com>
2025-04-20 04:36:27 +0000

    payments done

--- app.rb
+++ app.rb
@@ -24,3 +24,33 @@ get("/square_root/results") do
   @result = @input ** 0.5
   erb :square_root_results
 end
+
+get "/payment/new" do
+  erb :payment_new
+end
+
+get "/payment/results" do
+  @apr      = params[:user_apr].to_f
+  @apr_formatted = @apr.to_fs(:percentage, {:precision => 4})
+  r        = @apr / (100.0 * 12.0)
+  @years    = params[:user_years].to_i
+  n        = @years * 12
+  @pv       = params[:user_pv].to_f
+  @principal = @pv.to_fs(:currency)
+  @numerator   = r * @pv
+  @denominator = 1 - (1 + r)**(-n)
+  @payment     = @numerator / @denominator
+  @payment_formatted = @payment.to_fs(:currency)
+  erb :payment_results
+end
+
+get("/random/new") do
+  erb :random_new
+end
+
+get("/random/results") do
+  @min = params[:user_min].to_f
+  @max = params[:user_max].to_f
+  @random_number = rand(@min..@max)
+  erb :random_results
+end
