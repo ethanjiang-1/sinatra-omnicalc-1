61b4179458c222e8f9898d152971f6fdd6c8b98f
ethanjiang-1 <146405629+ethanjiang-1@users.noreply.github.com>
2025-04-20 03:25:51 +0000

    square(root) completed

--- app.rb
+++ app.rb
@@ -10,7 +10,17 @@ get("/square/new") do
 end
 
 get("/square/results") do
-  @input = params[:input].to_f
+  @input = params[:user_number].to_f
   @result = @input * @input
   erb :square_results
+end
+
+get("/square_root/new") do
+  erb :square_root_new
+end
+
+get("/square_root/results") do
+  @input = params[:user_number].to_f
+  @result = @input ** 0.5
+  erb :square_root_results
 end
